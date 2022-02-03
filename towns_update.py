6fed395208b129693fc382a607b1d832827ac5df
MC_cubes <mccube2000@outlook.com>
2022-02-03 18:37:17 +0800

    update

---
+++ towns_update.py
@@ -0,0 +1,63 @@
+# 城镇消失创建检测 by:MC_cubes
+
+import json
+import time
+import requests
+
+url = "https://earthmc-api.herokuapp.com/towns/"
+file = 'data.json'
+
+def read() -> list:
+    try:
+        with open(file, encoding='utf-8') as f:
+            js = json.load(f)
+            data = []
+            for i in js:
+                data.append({'name': i['name'], 'x': i['x'], 'z': i['z']})
+        return data
+    except:
+        with open(file, 'w', encoding='utf-8') as f:
+            data = []
+            json.dump(data, f)
+        return data
+
+
+def write(data: str) -> None:
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def update() -> list:
+    re = requests.get(url=url).content.decode("UTF-8")
+    js = json.loads(re)
+    data = []
+    for i in js:
+        data.append({'name': i['name'], 'x': i['x'], 'z': i['z']})
+    return data
+
+
+if __name__ == '__main__':
+    old = read()
+    ol = len(old)
+    try:
+        while True:
+            print(ol)
+            new = read()
+            nl = len(new)
+            if ol != nl:
+                # dis = [i for i in old if i not in new]
+                # cre = [i for i in new if i not in old]
+                for i in old:
+                    if i not in new:
+                        print(f'{i} 消失')
+                for i in new:
+                    if i not in old:
+                        print(f'{i} 创建')
+                old = new
+                ol = len(old)
+            print('sleep')
+            for i in range(10):
+                print('.')
+                time.sleep(1)
+    except:
+        write(old)
