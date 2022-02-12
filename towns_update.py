2f3153226eca8ef5bfbe45c975100da2339b94bc
MC_cubes <mccube2000@outlook.com>
2022-02-12 19:26:54 +0800

    显示地狱坐标& url可传入data.json

--- towns_update.py
+++ towns_update.py
@@ -7,11 +7,13 @@ import time
 
 import requests
 
-url = "https://earthmc-api.herokuapp.com/towns/"
+url = "http://earthmc-api.herokuapp.com/towns/"
 file = 'data.json'  # 数据文件目录
 pause = 10  # 暂停时间 秒 开启循环后生效
 loop = True  # 是否循环? False or True
 retry = 10  # 下载失败重试次数
+timeout = 30  # 下载超时时间
+show_speed = True  # 是否显示下载速度? False or True
 
 # logging.json 14 19 26行 level 可设置为 DEBUG INFO WARN
 #              21 28   行 filename 为输出文件，可写相对/绝对路径
@@ -23,7 +25,14 @@ logger = logging.getLogger("towns")
 
 
 def printr(plan: float, speed: str) -> None:
-    print('\r', '{:.1f}'.format(plan), '%  ', speed, end='    ', flush=True)
+    if show_speed:
+        print('\r',
+              '{:.1f}'.format(plan),
+              '%  ',
+              speed,
+              end='    ',
+              flush=True)
+
 
 retry_count = 0
 speed_info = [{
@@ -55,11 +64,9 @@ def download():
     global retry_count
     if retry_count < retry:
         logger.debug('正在获取数据大小......')
-        with requests.get(url, timeout=10, stream=True) as req:
-            # print(req.headers)
+        with requests.get(url, timeout=timeout, stream=True) as req:
             total_size = int(req.headers['content-length'])
             logger.debug(f'数据大小: {calc_size(total_size, False)}')
-            # logger.info(f"数据更新日期: {req.headers['Date']}")
             content_size = 0
             plan = 0
             temp_size = 0
@@ -70,6 +77,7 @@ def download():
                     data += content
                     content_size += len(content)
                     plan = (content_size / total_size) * 100
+                    speed = total_size
                     if time.time() - start_time > 1:
                         start_time = time.time()
                         speed = content_size - temp_size
@@ -100,7 +108,6 @@ def write(data: str) -> None:
 
 
 def update() -> dict:
-    logger.debug('正在获取城镇信息, 可能需要很久......')
     try:
         global retry_count
         retry_count = 0
@@ -108,10 +115,19 @@ def update() -> dict:
     except:
         logger.info('获取数据失败!')
         return None
-    data = {}
-    for i in js:
-        data[i['name']] = [i['x'], i['z']]
-    return data
+    data_dict = {}
+    try:
+        # 如果获取的是原网站数据, 取出需要的数据即可
+        for i in js:
+            data_dict[i['name']] = [i['x'], i['z']]
+        return data_dict
+    except:
+        # 如果获取的不是原网站数据, 上面会报错, 返回原始数据
+        return js
+
+
+def pos8(data):
+    return [data[0] / 8, data[1] / 8]
 
 
 if __name__ == '__main__':
@@ -126,10 +142,10 @@ if __name__ == '__main__':
             if ol != nl:
                 for i in old:
                     if i not in new:
-                        logger.warning(f'{i} {old[i]} 消失')
+                        logger.warning(f'{i} {old[i]} 消失 地狱位置：{pos8(old[i])}')
                 for i in new:
                     if i not in old:
-                        logger.info(f'{i} {new[i]} 创建')
+                        logger.info(f'{i} {new[i]} 创建 地狱位置：{pos8(new[i])}')
                 old = new
                 ol = len(old)
             if loop:
